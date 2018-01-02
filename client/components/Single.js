03a32978795fa15e6df0bc58b6664aba5950a75e
Liz Verde <>
2018-01-02 21:08:12 +0000

    The Comments component is now showing on the Single page - but needs to be styled

--- client/components/Single.js
+++ client/components/Single.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import Photo from './Photo.js';
+import Photo from './Photo';
 // import comments
+import Comments from './Comments'
 
 const Single = React.createClass({
   render() {
@@ -14,6 +15,7 @@ const Single = React.createClass({
     return (
     	<div className="single-photo">
     		<Photo i={ i } post={ post } { ...this.props } />
+    		<Comments />
     	</div>
     )
   }
