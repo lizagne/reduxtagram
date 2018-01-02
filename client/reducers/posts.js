ad2bbe74aea946a378d116545d14e073d26267f0
Liz Verde <>
2018-01-02 21:03:00 +0000

    Updating the Photo component into the Single screen page, the Photo component is duplicated

--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -5,12 +5,12 @@
 
 function posts(state = [], action) {
   switch(action.type) {
-    case 'INCREMENT_LIKES' :
+    case 'INCREMENT_LIKES' : //if it is this action then we return the updated state
       console.log("Incrementing Likes!!");
       const i = action.index;
       return [
         ...state.slice(0,i), // before the one we are updating
-        {...state[i], likes: state[i].likes + 1},
+        {...state[i], likes: state[i].likes + 1}, //return the new value
         ...state.slice(i + 1), // after the one we are updating
       ]
     default:
